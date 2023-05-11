27b891e574ec51203d24a50c90639a5ad35769ae
Ferhat ER <lersmooth@gmail.com>
2023-05-11 14:01:20 +0300

    added prev page functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 const page = document.querySelector("div.page.page-1");
 const nextPageButton = page.querySelector(".next-page-button");
+const prevPageButton = page.querySelector(".prev-page-button");
 
 nextPageButton.onclick = () => {
   page.classList.add("flipped");
+};
+prevPageButton.onclick = () => {
+  page.classList.remove("flipped");
 };
